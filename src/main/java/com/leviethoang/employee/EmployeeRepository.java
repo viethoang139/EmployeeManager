947978790172017535db7d6e3ed665aad918f0df
Le Viet Hoang <levieth710@gmail.com>
2023-01-29 12:01:01 +0700

    update EmployeeRepository and search_result.html

--- src/main/java/com/leviethoang/employee/EmployeeRepository.java
+++ src/main/java/com/leviethoang/employee/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.leviethoang.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee , Integer> {
     public Long countById(Integer id);
 
